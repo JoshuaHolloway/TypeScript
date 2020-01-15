ef1ec3e8992c469eccdd33dd8253917b64193b40
joshuaholloway <jhollow6@asu.edu>
2020-01-15 17:29:58 -0600

    Template Strings

---
+++ 11/index.js
@@ -0,0 +1,7 @@
+// > tsc --watch index.ts
+// ---------- TEMPLATE STRINGS ----------
+// Create multiline strings
+var multStr = "I go on for\nmany lines<br />";
+document.write(multStr);
+// You can use string interpolation
+document.write("<b>" + multStr + "</b>");
